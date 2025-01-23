0b2099610677ed36ed916925716cb85622216179
Mohammad Naim Faizy <mnaim.faizy@gmail>
2025-01-23 12:54:38 +1100

    adding authentication using Next-auth library

---
+++ app/login/page.tsx
@@ -0,0 +1,25 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import LoginForm from '@/app/ui/login-form';
+import { Suspense } from 'react';
+ 
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className='text-gray-300 font-bold text-center mb-2'>
+            You can access the dashboard with the following credentials: <br />
+            Email: <span className='text-gray-500'>user@nextmail.com</span> <br />
+            Password: <span className='text-gray-500'>123456</span>
+        </div>
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <Suspense>
+          <LoginForm />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
